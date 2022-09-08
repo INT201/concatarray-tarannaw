e034c7c40b0047a22871f877d65830cf38f22866
Wannarat Pumpipat <wannarat.pump@kmutt.ac.th>
2022-09-08 18:34:34 +0700

    concat V1

--- concatArray.js
+++ concatArray.js
@@ -1,6 +1,12 @@
 const { template } = require('@babel/core')
 
 function concatArray(array1, array2) {
-  //code here
+  if (array1 === null || array2 === undefined && array1 === undefined || array2 === null) {
+    return undefined
+  } else if (array1 === [] && array2 === []) {
+    return (array1 || array2)
+  } else if (array1 === [] || array2 === [] || array1 === null || array2 === null || array1 === undefined || array2 === undefined) {
+    return array1.concat(array2)
+  }
 }
 module.exports = concatArray
