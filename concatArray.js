665ff78b6676b81aa49b60cf6aebb4a6a6a4671b
Wannarat Pumpipat <wannarat.pump@kmutt.ac.th>
2022-09-08 18:38:37 +0700

    concat V2

--- concatArray.js
+++ concatArray.js
@@ -5,7 +5,11 @@ function concatArray(array1, array2) {
     return undefined
   } else if (array1 === [] && array2 === []) {
     return (array1 || array2)
-  } else if (array1 === [] || array2 === [] || array1 === null || array2 === null || array1 === undefined || array2 === undefined) {
+  } else if (array1 === [] || array1 === null || array1 === undefined) {
+    return array1
+  } else if (array2 === [] || array2 === null || array2 === undefined) {
+    return array2
+  } else {
     return array1.concat(array2)
   }
 }
